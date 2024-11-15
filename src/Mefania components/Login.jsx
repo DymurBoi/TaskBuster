67f288cf693a73d7369b97605da093748ba353b2
Redgel! <redgelgregory.mefania@cit.edu>
2024-11-15 14:48:43 +0800

    added password features
    added password features:
    visibility toggle for password
    and
    Correct Password convention

--- src/Mefania components/Login.jsx
+++ src/Mefania components/Login.jsx
@@ -2,15 +2,41 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import './css.css';
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState('');
   const navigate = useNavigate();
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+
+    // Password validation regex
+    const passwordRegex = /^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    if (!passwordRegex.test(value)) {
+      setPasswordError('Password must be at least 8 characters and contain at least one special character.');
+    } else {
+      setPasswordError('');
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    if (passwordError) {
+      alert('Please enter a valid password.');
+      return;
+    }
+
     try {
       const response = await axios.post(`http://localhost:8080/api/user/login?email=${email}&password=${password}`);
       const { userId, token } = response.data;
@@ -51,14 +77,22 @@ const Login = ({ onLogin }) => {
               required
               className="input"
             />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="input"
-            />
+            <div className="password-container">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+                className="input"
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEye : faEyeSlash}
+                className="eye-icon"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+            {passwordError && <p className="error">{passwordError}</p>}
             <button type="submit" className="button">Login</button>
           </form>
         </div>
